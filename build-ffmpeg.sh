8261ef971571192b0c8b9d3045c3ab526e5ef9f6
huangpeng <huangpeng@chunyu.me>
2017-02-17 13:18:58 +0800

    build for audio only

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -14,12 +14,90 @@ MP3='mp3'
 
 #FDK_AAC=`pwd`/fdk-aac/fdk-aac-ios
 
-CONFIGURE_FLAGS="--enable-cross-compile --disable-debug --disable-programs \
-                 --disable-doc --enable-pic"
+CONFIGURE_FLAGS="--enable-cross-compile \
+                 --disable-debug \
+                 --disable-programs \
+                 --disable-doc \
+                   --enable-pic \
+                   --enable-memalign-hack \
+        --disable-shared \
+        --enable-static \
+        --disable-avdevice \
+        --disable-avfilter \
+        --disable-swscale \
+        --enable-ffmpeg \
+        --disable-ffplay \
+        --disable-ffserver \
+        --disable-network \
+        --disable-muxers \
+        --disable-demuxers \
+        --enable-rdft \
+        --enable-demuxer=aac \
+        --enable-demuxer=ac3 \
+        --enable-demuxer=ape \
+        --enable-demuxer=asf \
+        --enable-demuxer=flac \
+        --enable-demuxer=matroska_audio \
+        --enable-demuxer=mp3 \
+        --enable-demuxer=mpc \
+        --enable-demuxer=mov \
+        --enable-demuxer=mpc8 \
+        --enable-demuxer=ogg \
+        --enable-demuxer=tta \
+        --enable-demuxer=wav \
+        --enable-demuxer=wv \
+        --disable-bsfs \
+        --disable-filters \
+        --disable-parsers \
+        --enable-parser=aac \
+        --enable-parser=ac3 \
+        --enable-parser=mpegaudio \
+        --disable-protocols \
+        --enable-protocol=file \
+        --disable-indevs \
+        --disable-outdevs \
+        --disable-encoders \
+        --disable-decoders \
+        --enable-decoder=aac \
+        --enable-decoder=ac3 \
+        --enable-decoder=alac \
+        --enable-decoder=ape \
+        --enable-decoder=flac \
+        --enable-decoder=mp1 \
+        --enable-decoder=mp2 \
+        --enable-decoder=mp3 \
+        --enable-decoder=mpc7 \
+        --enable-decoder=mpc8 \
+        --enable-decoder=tta \
+        --enable-decoder=vorbis \
+        --enable-decoder=wavpack \
+        --enable-decoder=wmav1 \
+        --enable-decoder=wmav2 \
+        --enable-decoder=pcm_alaw \
+        --enable-decoder=pcm_dvd \
+        --enable-decoder=pcm_f32be \
+        --enable-decoder=pcm_f32le \
+        --enable-decoder=pcm_f64be \
+        --enable-decoder=pcm_f64le \
+        --enable-decoder=pcm_s16be \
+        --enable-decoder=pcm_s16le \
+        --enable-decoder=pcm_s16le_planar \
+        --enable-decoder=pcm_s24be \
+        --enable-decoder=pcm_daud \
+        --enable-decoder=pcm_s24le \
+        --enable-decoder=pcm_s32be \
+        --enable-decoder=pcm_s32le \
+        --enable-decoder=pcm_s8 \
+        --enable-decoder=pcm_u16be \
+        --enable-decoder=pcm_u16le \
+        --enable-decoder=pcm_u24be \
+        --enable-decoder=pcm_u24le \
+        --enable-decoder=rawvideo"
 
 if [ "$MP3" ]
 then
-	CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-libmp3lame"
+	#CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-libmp3lame"
+  echo "hello"
 fi
 
 if [ "$X264" ]
@@ -35,7 +113,7 @@ fi
 # avresample
 #CONFIGURE_FLAGS="$CONFIGURE_FLAGS --enable-avresample"
 
-ARCHS="arm64 armv7 x86_64 i386"
+ARCHS="arm64 armv7 armv7s x86_64 i386"
 
 COMPILE="y"
 LIPO="y"
